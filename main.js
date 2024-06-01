5a2542b8bc62dec7fc321c91cf4c85757ee3418f
albert19_ <albertnanda1902@gmail.com>
2024-06-01 15:55:28 +0800

    configure devtools and add electronmon to auto reload changes

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 const path = require("path");
 const { app, BrowserWindow } = require("electron");
 
+const isDev = process.env.NODE_ENV !== "development";
+const isMac = process.platform === "darwin";
+
 const createMainWindow = () => {
   const mainWindow = new BrowserWindow({
     title: "Aplikasi Manajemen Keuangan UKM",
-    width: 800,
-    height: 600,
+    width: isDev ? 1000 : 500,
+    height: 900,
   });
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 
   mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 };
 
 app.whenReady().then(() => {
   createMainWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
 });
